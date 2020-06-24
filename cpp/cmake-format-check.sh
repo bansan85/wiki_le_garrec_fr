48bff9dc9fe7e2d8b86cad7f5ab536f065d3e724
Le Garrec Vincent <legarrec.vincent@gmail.com>
2020-06-24 13:13:37 +0200

    Fix warnings from cmake-format

--- cpp/cmake-format-check.sh
+++ cpp/cmake-format-check.sh
@@ -1,2 +1,9 @@
 $(cmake-format $1 | diff $1 -)
-if [ $? -ne "0" ]; then cmake-format $1 | diff -pu $1 - ; exit 1; fi
+if [ $? -ne "0" ]
+then
+  cmake-format $1 | diff -pu $1 -
+  exit 1
+else
+  touch $1.stamp
+  exit 0
+fi
