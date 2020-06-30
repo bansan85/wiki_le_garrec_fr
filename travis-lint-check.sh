beddec989784fd881a2b2fef72c6354fbb31e868
Le Garrec Vincent <legarrec.vincent@gmail.com>
2020-06-30 19:27:52 +0200

    Use --pro for both travis login and lint
    I had randomly once :
    
    $ travis login --pro --github-token $GITHUB_PERSONAL_TOKEN
    The command "travis login --pro --github-token $GITHUB_PERSONAL_TOKEN" exited with 0.
    $ bash ./travis-lint-check.sh
    travis/helloworld.yml
    valid
    .travis.yml
    invalid access token - try running travis login
    The command "bash ./travis-lint-check.sh" exited with 1.
    
    Let's try this.

--- travis-lint-check.sh
+++ travis-lint-check.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Need bash for shopt
 
+travis login --pro --github-token $GITHUB_PERSONAL_TOKEN || exit 1
+
 shopt -s globstar dotglob
 for i in **/*.yml; do
   echo $i
-  travis lint "$i" -x --no-interactive || exit 1
+  travis lint --pro "$i" -x --no-interactive || exit 1
 done
 
 exit 0
