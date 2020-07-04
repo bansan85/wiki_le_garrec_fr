343c8ee614b7acbe7806160b5aa88d0e54627458
Le Garrec Vincent <legarrec.vincent@gmail.com>
2020-07-05 00:49:57 +0200

    Fix shellcheck

--- cpp/clang-format-check.sh
+++ cpp/clang-format-check.sh
@@ -1,1 +1,6 @@
-if [ $(clang-format -style=file $1 -output-replacements-xml |grep "<replacement offset=" | wc -l) -ne "0" ]; then clang-format -style=file $1 | diff -pu $1 - ; exit 1; fi
+#!/bin/sh
+
+if [ "$(clang-format -style=file "$1" -output-replacements-xml |grep -c \"<replacement offset=\")" -ne "0" ]; then
+  clang-format -style=file "$1" | diff -pu "$1" - ;
+  exit 1;
+fi
--- cpp/cmake-format-check.sh
+++ cpp/cmake-format-check.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo "Checking format $1"
-$(cmake-format $1 --check)
-if [ $? -ne "0" ]
+
+if ! cmake-format "$1" --check;
 then
-  cmake-format $1 | diff -pu $1 -
+  cmake-format "$1" | diff -pu "$1" -
   exit 1
 else
-  touch $1.stamp
+  touch "$1.stamp"
   exit 0
 fi
