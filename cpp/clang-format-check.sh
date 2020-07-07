af4423eed7ae0b79d3c072518d8f637fc849fcd1
Le Garrec Vincent <legarrec.vincent@gmail.com>
2020-07-07 22:05:15 +0200

    -Wno-gnu-zero-variadic-macro-arguments
    must specify at least one argument for '...' parameter of variadic macro
    
    +fix clang-format

--- cpp/clang-format-check.sh
+++ cpp/clang-format-check.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$(clang-format -style=file "$1" -output-replacements-xml |grep -c \"<replacement offset=\")" -ne "0" ]; then
+if [ "$(clang-format -style=file "$1" -output-replacements-xml |grep -c "<replacement offset=")" -ne "0" ]; then
   clang-format -style=file "$1" | diff -pu "$1" - ;
   exit 1;
 fi
--- cpp/preprocessor/bracket.cpp
+++ cpp/preprocessor/bracket.cpp
@@ -5,7 +5,11 @@
 
 #include <list>
 
-void f(std::list<int>) {}
+namespace
+{
+// NOLINTNEXTLINE(readability-named-parameter)
+void f(const std::list<int>&) {}
+}  // namespace
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define F(XXXX, ...) f(XXXX)
 
@@ -13,21 +17,23 @@ int main()
 {
   std::list<int> c;
   f(c);
-  f(std::list<int>{ 1, 2, 3, 4 });
-  f({ 1, 2, 3, 4 });
+  // NOLINTNEXTLINE(fuchsia-default-arguments-calls)
+  f(std::list<int>{1, 2, 3, 4});
+  // NOLINTNEXTLINE(fuchsia-default-arguments-calls)
+  f({1, 2, 3, 4});
 
   // Failed. Macro stops at first comma.
-  STR(F(std::list<int>{ 1, 2, 3, 4 }));
+  STR(F(std::list<int>{1, 2, 3, 4}));
 
   // OK
   STR(F((std::list<int>{1, 2, 3, 4})));
 
   // OK
-  STR(F(std::list<int>({ 1, 2, 3, 4 })));
+  STR(F(std::list<int>({1, 2, 3, 4})));
 
   // Failed. Macro stops at first comma.
-  STR(F({ 1, 2, 3, 4 }));
+  STR(F({1, 2, 3, 4}));
 
   // Failed. With double parentheses, c++ can't auto deduce std::list<std::string>.
-  STR(F(({ 1, 2, 3, 4 })));
+  STR(F(({1, 2, 3, 4})));
 }
