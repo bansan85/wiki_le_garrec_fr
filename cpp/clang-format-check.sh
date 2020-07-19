059c91c1bd66339393095ca12425c6501f71fb56
Le Garrec Vincent <legarrec.vincent@gmail.com>
2020-07-19 09:45:41 +0200

    Improve scripts to be able to run them locally
    Part 1

--- cpp/clang-format-check.sh
+++ cpp/clang-format-check.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 if [ "$(clang-format -style=file "$1" -output-replacements-xml |grep -c "<replacement offset=")" -ne "0" ]; then
-  clang-format -style=file "$1" | diff -pu "$1" - ;
-  exit 1;
+  if [ "$CI" = "true" ] && [ "$TRAVIS" = "true" ]
+  then
+    clang-format -style=file "$1" | diff -pu "$1" -
+    exit 1
+  else
+    clang-format -style=file -i "$1"
+    exit 0
+  fi
 fi
+exit 0
